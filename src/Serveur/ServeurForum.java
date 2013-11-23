7b9d02857191df2eb04b20d406e31547e11c7be8
walllzzz <noutest@gmail.com>
2013-11-23 15:36:22 +0100

    difinition du serveur

--- src/Serveur/ServeurForum.java
+++ src/Serveur/ServeurForum.java
@@ -4,10 +4,52 @@
  */
 package Serveur;
 
+import Commun.InterfaceServeurForum;
+import Commun.InterfaceSujetDiscussion;
+import java.awt.Frame;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author mon pc
  */
-public class ServeurForum {
+public class ServeurForum extends UnicastRemoteObject implements InterfaceServeurForum{
+    ArrayList<InterfaceSujetDiscussion> sujets;
+    public ServeurForum() throws RemoteException{
+        sujets=new ArrayList();
+    }
     
+    @Override
+    public InterfaceSujetDiscussion obtientSujet(String titre) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+    public static void main(String args[]) {
+    try {
+      // creation du serveur de forum et enregistrement sur le reseau
+      LocateRegistry.createRegistry(8686);
+      InterfaceServeurForum server = new ServeurForum();
+      String URL="localhost";
+      Naming.bind(URL, server);
+      System.out.println("Démarrage du serveur...");
+
+      JOptionPane.showMessageDialog(new Frame(),
+          "Quit this panel to close the server.", "Server Connected",
+          JOptionPane.WARNING_MESSAGE);
+      System.exit(0);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  
 }
--- src/Serveur/SujetDiscussion.java
+++ src/Serveur/SujetDiscussion.java
@@ -4,10 +4,33 @@
  */
 package Serveur;
 
+import Commun.InterfaceAffichageClient;
+import Commun.InterfaceSujetDiscussion;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
 /**
  *
  * @author mon pc
  */
-public class SujetDiscussion {
+public class SujetDiscussion implements InterfaceSujetDiscussion{
+    String sujet;
+    ArrayList<InterfaceAffichageClient> clients;
+    @Override
+    public void inscription(InterfaceAffichageClient c) throws RemoteException {
+        clients.add(c);
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void desincription(InterfaceAffichageClient c) throws RemoteException {
+        clients.remove(c);
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void diffuse(String message) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
